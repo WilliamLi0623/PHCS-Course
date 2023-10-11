15d2384e8248c4e966c97ab0970178d7d268bfa5
WilliamLi0623 <61577379+WilliamLi0623@users.noreply.github.com>
2023-10-11 10:47:07 +0800

    First Lecture

---
+++ CS101/Code/Lecture 1/A+B Problem.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+
+int main(){
+    int qwq=0,qaq=0;
+    scanf("%d%d",&qwq,&qaq);
+    printf("%d\n",qwq+qaq);
+    return 0;
+}
---
+++ CS101/Code/Lecture 1/Hello World.cpp
@@ -0,0 +1,7 @@
+#include<cstdio>
+// #include<bits/stdc++.h>
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
